88a5c0185294047401778a6e36a8758b606704b9
JeffreyWang <admin@wj2015.com>
2019-11-12 21:17:04 +0800

    doc:文档

--- 2019-11 Python基础分享/code/rabbit-1.py
+++ 2019-11 Python基础分享/code/rabbit-1.py
@@ -1,8 +1,6 @@
-a = 1
-if a > 1:
-	print('> 1')
-else:
-	if a == 1:
-		print('= 1')
-	else:
-		print('< 1')
+arr = [1, 1]
+day = 12
+
+for i in range(2, day):
+    arr.append(arr[i - 2] + arr[i - 1])
+print(arr)
